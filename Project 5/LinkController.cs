d93b6a96a672dbf1d8dab2b0f30805d9157aec9d
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-10-18 19:11:46 -0400

    Revert "Created a working test implementation of the HTTP endpoint"
    This reverts commit 5b7df853edc9250e23ee85651b321da6f0228925.

--- Project 5/LinkController.cs
+++ Project 5/LinkController.cs
@@ -1,46 +1,5 @@
-﻿using System.Net;
-using System.Net.Sockets;
-
-namespace link
+﻿
+class LinkController
 {
-    public class LinkController
-    {
-        public void CreateEndpoints()
-        {
-            string[] endpoints = { "http://localhost:2400/", "http://localhost:2400/status/" };
-
-            HttpListener listener = new HttpListener();
-
-            foreach (string endpoint in endpoints)
-            {
-                listener.Prefixes.Add(endpoint);
-            }
-            listener.Start();
-            Console.WriteLine(listener.ToString());
-
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest req = context.Request;
-
-            Console.WriteLine(req.RawUrl);
-            switch(req.RawUrl)
-            {
-                case "/status/":
-                    Console.WriteLine("client requested status");
-                    break;
-            }
-            HttpListenerResponse res = context.Response;
-
-            string responseString = "Bruh";
-
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-
-            res.ContentLength64 = buffer.Length;
-            System.IO.Stream output = res.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-
-            output.Close();
-            listener.Stop();
-        }
-    }
 
 }
