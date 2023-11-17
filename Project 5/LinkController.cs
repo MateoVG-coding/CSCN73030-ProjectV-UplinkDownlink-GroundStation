ab4cc324d2fe5e91550f40ab91393243fdcafa63
Alistair Campbell <AJCampbell@conestogac.on.ca>
2023-11-17 10:27:20 -0500

    Fixed broken tests

--- Project 5/LinkController.cs
+++ Project 5/LinkController.cs
@@ -33,7 +33,7 @@ namespace link
             }
             else
             {
-                //throw new Exception(BandwidthLimitReachedException);
+                throw new OutOfBandwidthException(bandwidth, messageSize);
             }
         }
 
@@ -136,4 +136,16 @@ namespace link
         }
 
     }
+
+    public class OutOfBandwidthException : Exception
+    {
+        public OutOfBandwidthException() { }
+
+        public OutOfBandwidthException(int currentBandwidth, int attemptedBandwidth)
+            : base(String.Format("Tried to use more than available bandwidth ", attemptedBandwidth, " of remaining ", currentBandwidth))
+        {
+
+        }
+    }
+
 }
--- Unit Tests/LinkController.Tests.cs
+++ Unit Tests/LinkController.Tests.cs
@@ -10,48 +10,57 @@ namespace Unit_Tests
     [TestClass]
     public class LinkControllerTests
     {
-        [TestClass]
-        public class APITests
-        {
-            [TestMethod]
-            public void LinkController_Status_Get()
-            {
+        //[TestClass]
+        //public class APITests
+        //{
+        //    [TestMethod]
+        //    public void LinkController_Status_Get()
+        //    {
 
-            }
-            [TestMethod]
-            public void LinkController_Status_Other()
-            {
+        //    }
+        //    [TestMethod]
+        //    public void LinkController_Status_Other()
+        //    {
 
-            }
-            [TestMethod]
-            public void LinkController_Send_Post() { }
-            [TestMethod]
-            public void LinkController_Send_Other() { }
-            [TestMethod]
-            public void LinkController_Receive_Post() { }
-            [TestMethod]
-            public void LinkController_Receive_Other() { }
-            [TestMethod]
-            public void LinkController_Handle_Other()
-            {
+        //    }
+        //    [TestMethod]
+        //    public void LinkController_Send_Post() { }
+        //    [TestMethod]
+        //    public void LinkController_Send_Other() { }
+        //    [TestMethod]
+        //    public void LinkController_Receive_Post() { }
+        //    [TestMethod]
+        //    public void LinkController_Receive_Other() { }
+        //    [TestMethod]
+        //    public void LinkController_Handle_Other()
+        //    {
 
-            }
-        }
+        //    }
+        //}
 
         [TestClass]
         public class bandwidthTests
         {
             [TestMethod]
-            public void getBandwidth() 
+            public void getBandwidthTest() 
             {
                 LinkController controller = new LinkController();
-                Assert.AreEqual(0, controller.getBandwidth());
+                Assert.AreEqual(35000, controller.getBandwidth());
             }
             [TestMethod]
-            public void addBandwidth()
+            public void addBandwidthTest()
             {
                 LinkController controller = new LinkController();
                 controller.addBandwidth(100);
+
+                Assert.AreEqual(34900, controller.getBandwidth());
+            }
+            [TestMethod]
+            public void addBandwidthErrorTest()
+            {
+                LinkController controller = new LinkController();
+                
+                Assert.ThrowsException<OutOfBandwidthException>(() => controller.addBandwidth(36000));
             }
         }
 
