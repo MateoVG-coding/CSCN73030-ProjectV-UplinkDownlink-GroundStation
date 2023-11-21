f1f0c169caf2e814b286a9785193f19721a96319
Jose Ibanez Polo <Jibanezpolo3629@conestogac.on.ca>
2023-11-20 21:10:08 -0500

    Integration test updated once again

--- Integ-Uplink-SpaceSender/IntegTests.cs
+++ Integ-Uplink-SpaceSender/IntegTests.cs
@@ -11,12 +11,10 @@ namespace Integ_Uplink_SpaceSender
             //Arrange
             String fakeAddress = "192.168.1.1";
             String fakeEndpointSpace = "https://httpbin.org/post";
-            String fakeEndPointPassthrough = "https://httpbin.org/post";
 
             Queue<String> testQueue = Stub_SpaceSender.GetFakedTransmissionQuueue();
             Mutex bufferlock = new Mutex();
             SpaceSender space = new SpaceSender(fakeEndpointSpace, ref testQueue, ref bufferlock);
-            SpaceSender passthrough = new SpaceSender(fakeEndPointPassthrough, ref testQueue, ref bufferlock);
             Uplink_MadeMockable link = new Uplink_MadeMockable(fakeAddress, fakeEndpointSpace, ref space);
 
             //Act
